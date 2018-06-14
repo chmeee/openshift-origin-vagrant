89cc50469770acfdc5ae27911e3dae1a12542a20
José Manuel Pavón Álvarez <jmpavon@intellimentsec.com>
2018-06-14 15:07:16 +0100

    Fixed multiline sed

--- vagrant_up.sh
+++ vagrant_up.sh
@@ -12,10 +12,12 @@ for n in $(seq 1 $NODECOUNT); do
   vagrant up origin-node-$n --color <<< 'boot' || EXIT=$?
 done
 
+if [ $EXIT != 0 ]; echo "Vagrant error, no inventory file will be created"; exit $EXIT; fi
+
 MASTERIP=$(vagrant ssh-config origin-master | grep -Po '\d+\.\d+\.\d+\.\d+')
 
-sed -e "s#USERHOME#$HOME#;\\
-	s#MASTERIP#$MASTERIP#; \\
+sed -e "s#USERHOME#$HOME#;
+	s#MASTERIP#$MASTERIP#;
 	s#NODECOUNT#$NODECOUNT#" inventory-template > inventory
 
 exit $EXIT
