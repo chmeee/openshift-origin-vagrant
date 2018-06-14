f7bdc1e300e89b81367684d201890672e9dc6a55
José Manuel Pavón Álvarez <jmpavon@intellimentsec.com>
2018-06-14 17:10:19 +0100

    Memory limit added to inventory and some fixes

--- vagrant_up.sh
+++ vagrant_up.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 NODECOUNT=$(grep ^NODECOUNT= Vagrantfile | cut -f2 -d=)
+
+# If you have different node memory sizes, you'll need to set this option manually to the least
+MEMORY=$(( $(grep ^MEMORY= Vagrantfile | cut -f2 -d=) / 1024 ))
 
 EXIT=0
 
@@ -12,12 +15,13 @@ for n in $(seq 1 $NODECOUNT); do
   vagrant up origin-node-$n --color <<< 'boot' || EXIT=$?
 done
 
-if [ $EXIT != 0 ]; echo "Vagrant error, no inventory file will be created"; exit $EXIT; fi
+if [[ $EXIT != 0 ]]; then echo "Vagrant up error, no inventory file will be created"; exit $EXIT; fi
 
 MASTERIP=$(vagrant ssh-config origin-master | grep -Po '\d+\.\d+\.\d+\.\d+')
 
+if [[ -z $MASTERIP ]]; then echo "Couldn't get master IP, no inventory file will be created"; exit $EXIT; fi
+
 sed -e "s#USERHOME#$HOME#;
+	s#MEMORY#$MEMORY#;
 	s#MASTERIP#$MASTERIP#;
 	s#NODECOUNT#$NODECOUNT#" inventory-template > inventory
-
-exit $EXIT
