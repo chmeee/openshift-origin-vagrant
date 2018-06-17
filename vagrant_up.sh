57c179732ca32b5676782f9245d058105a006b9e
chmeee <chmeee@gmail.com>
2018-06-17 20:02:16 +0200

    Fixed echo with comments

--- vagrant_up.sh
+++ vagrant_up.sh
@@ -32,6 +32,6 @@ ansible-playbook ./playbooks/byo/config.yml -i ../openshift-origin-vagrant/inven
 cd -
 vagrant ssh origin-master -c 'sudo cat /etc/origin/master/admin.kubeconfig' > admin.kubeconfig
 
-echo # Execute the following to have oc configured with the openshift cluster for this shell
-echo # Next time you can also eval this script
+echo "# Execute the following to have oc configured with the openshift cluster for this shell"
+echo "# Next time you can also eval this script"
 echo export KUBECONFIG=$PWD/admin.kubeconfig
