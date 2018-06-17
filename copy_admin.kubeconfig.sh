81e07d348039866639c4d2cdc564be6664caa3e7
chmeee <chmeee@gmail.com>
2018-06-17 18:49:53 +0200

    Simplified admin.kubeconfig copy

--- copy_admin.kubeconfig.sh
+++ copy_admin.kubeconfig.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-MASTERIP=$(vagrant ssh-config origin-master | grep -Po '\d+\.\d+\.\d+\.\d+')
-
-if [[ -z $MASTERIP ]]; then echo "Couldn't get master IP, can't copy the admin.kubeconfig file"; exit $EXIT; fi
-
-ssh -i ~/.vagrant.d/insecure_private_key vagrant@10.255.1.158 'sudo cat /etc/origin/master/admin.kubeconfig' > admin.kubeconfig
+vagrant ssh origin-master -c 'sudo cat /etc/origin/master/admin.kubeconfig' > admin.kubeconfig
 
 echo # Execute the following to have oc configured with the openshift cluster for this shell
 echo # Next time you can also eval this script
