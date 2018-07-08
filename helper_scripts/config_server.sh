1dca4d993ca7e7309ab40735dd91ae44ed442206
chmeee <chmeee@gmail.com>
2018-07-08 10:15:01 +0200

    Adds netstat installation

--- helper_scripts/config_server.sh
+++ helper_scripts/config_server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ## Install network analysis tools
-yum install -y tcpdump wireshark nmap-ncat
+yum install -y tcpdump wireshark nmap-ncat netstat
 
 ## Convenience script to use ovs commands on the openvswitch container
 mkdir /home/vagrant/bin
